dd6406d4a82aa2de3f21369516b4c36e7085f082
brando2002 <136311894+brando2002@users.noreply.github.com>
2023-06-12 15:55:13 +0800

    Add files via upload
    uhgb

---
+++ Scripts.js
@@ -0,0 +1,15 @@
+
+function redirectToLink() {
+  window.location.href = "https://www.google.com"; // Replace with the desired link
+}
+
+
+var hideButton = document.getElementById("skills");
+var elements = document.getElementsByClassName("skillz");
+
+
+hideButton.addEventListener("click", function() {
+
+    element.style.display = "none";
+  
+});
